7dad64b1d9b30e3e9be4e48aa8e54c2d84be1f74
Serhii Yahdzhyiev <syahdzhyiev@gmail.com>
2025-02-17 16:11:30 +0100

    Remove mock log messages.

--- extension/background.js
+++ extension/background.js
@@ -1,5 +1,4 @@
 importScripts("./api.js");
 
 (async () => {
-    console.log("Hello from CodeGlass background worker!");
 })();
--- extension/content.js
+++ extension/content.js
@@ -1,5 +1,3 @@
-console.log("Hello from CodeGlass content!");
-
 chrome.runtime.onMessage.addListener(async (message, _, sendResponse) => {
     if (!message) return;
     switch(message.action) {

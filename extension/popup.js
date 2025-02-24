d6371034764ae6a6dcbc12ade7451371f93f4b1e
Serhii Yahdzhyiev <syahdzhyiev@gmail.com>
2025-02-24 14:07:57 +0100

    Improve extensions popup.
    Add header, footer, and control buttons.

--- extension/popup.js
+++ extension/popup.js
@@ -1,16 +1,38 @@
-(async () => {
-    let secretValue = (await chrome.storage.local.get())["SECRET"];
+const editBtn = document.getElementById("edit");
+const saveBtn = document.getElementById("save");
 
+editBtn.addEventListener("click", handleEdit);
+saveBtn.addEventListener("click", handleSave);
+
+async function handleSave(e) {
+    Array.from(document.querySelectorAll("footer button"))
+        .forEach((button) => button.disabled = false);
+    Array.from(document.querySelectorAll("input"))
+        .forEach((input) => input.disabled = true);
+
+    const input = document.getElementById("secret");
+    await chrome.storage.local.set({"SECRET": input.value});
+
+    e.target.disabled = true;
+}
+
+function handleEdit(e) {
+    Array.from(document.querySelectorAll("footer button"))
+        .forEach((button) => button.disabled = false);
+    Array.from(document.querySelectorAll("input"))
+        .forEach((input) => input.disabled = false);
+
+    e.target.disabled = true;
+}
+
+chrome.storage.local.get().then(storage => {
+    const secretValue = storage["SECRET"];
     if (!secretValue) {
         console.warn("No secret obtained from storage!");
         secretValue = "secret";
     }
-
     const input = document.getElementById("secret");
-
     input.value = secretValue;
+});
 
-    input.addEventListener("change", async (e) => {
-        await chrome.storage.local.set({"SECRET": e.target.value});
-    })
-})();
+
